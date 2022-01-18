c694939b256805cd13c4565f2a4e7aed44194d50
gefgu <gefgu@hotmail.com>
2022-01-18 10:43:26 -0300

    Add format with prettier

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function add(...values) {
-    return values.reduce((sum, currentValue) => sum + (+currentValue), 0);
+  return values.reduce((sum, currentValue) => sum + +currentValue, 0);
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(...values) {
-    return values.reduce((product, currentValue) => product * (+currentValue), 1);
+  return values.reduce((product, currentValue) => product * +currentValue, 1);
 }
 
 function divide(a, b) {
-    return a/b;
+  return a / b;
 }
 
 function operate(operator, a, b) {
-    if (operator == "+") {
-        return add(a, b);
-    } else if (operator == "-") {
-        return subtract(a, b);
-    } else if (operator == "*") {
-        return multiply(a, b);
-    } else if (operator == "/") {
-        return divide(a, b);
-    } 
+  if (operator == "+") {
+    return add(a, b);
+  } else if (operator == "-") {
+    return subtract(a, b);
+  } else if (operator == "*") {
+    return multiply(a, b);
+  } else if (operator == "/") {
+    return divide(a, b);
+  }
 }
